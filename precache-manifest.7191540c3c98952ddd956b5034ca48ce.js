adb4ccc79c68e604db0d61e7280257c1e1458d4d
Pietralik <pawel.pietralik@bauer.pl>
2019-10-29 08:37:38 +0100

    Updates

---
+++ precache-manifest.7191540c3c98952ddd956b5034ca48ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b9c24c426c8a6409a287645dc50d05",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4109658fddb8d250fcd5",
+    "url": "/monsters-rolodex/static/css/main.0a201537.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "4109658fddb8d250fcd5",
+    "url": "/monsters-rolodex/static/js/main.48c14ccb.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
